a419a708b895d283b7ff04eaca4ecc9b19049d66
Ashton Hauser <ashton.hauser777@gmail.com>
2018-11-28 20:30:59 +0000

    Now shows current user in chatbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React, {Component} from 'react';
 import {MessageList} from './MessageList.jsx';
-import {ChatBar} from './ChatBar.jsx'
+import {ChatBar} from './ChatBar.jsx';
+import allMsgs from '../data/messages.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return  (
-      <div className="app-container">
-        <nav className="navbar">
-          <a href="/" className="navbar-brand">Chatty</a>
-        </nav>
-        <MessageList />
-        <ChatBar />
-      </div>
-    );
+    return (<div className="app-container">
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">Chatty</a>
+      </nav>
+      <MessageList messages={allMsgs} />
+      <ChatBar currentUser="Bob123" />
+    </div>);
   }
 }
 
